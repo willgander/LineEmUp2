12f8485daa99d07772fce546f35bb8cf84a21fa5
willgander <willgoldman99@gmail.com>
2022-11-05 10:27:17 -0400

    Level1Basic

---
+++ Assets/WillScripts/ClickAndDrag.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    private Plane dragPlane;
+
+    private Vector3 offset;
+
+    private Camera myMainCamera;
+
+    void Start()
+    {
+        myMainCamera = Camera.main; 
+    }
+
+    void OnMouseDown()
+    {
+        dragPlane = new Plane(myMainCamera.transform.forward, transform.position);
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        offset = transform.position - camRay.GetPoint(planeDist);
+    }
+
+    void OnMouseDrag()
+    {
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        transform.position = camRay.GetPoint(planeDist) + offset;
+    }
+}
---
+++ Assets/WillScripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject tile1;
+    public GameObject tile2;
+    public GameObject tile3;
+    public GameObject tile4;
+    public GameObject tile5;
+    public GameObject winPanel;
+
+    private void FixedUpdate()
+    {
+        if (tile1.transform.position.x < tile2.transform.position.x)
+        {
+            
+            if (tile2.transform.position.x < tile3.transform.position.x)
+            {
+                if (tile3.transform.position.x < tile4.transform.position.x)
+                {
+                    if (tile4.transform.position.x < tile5.transform.position.x)
+                    {
+                        win();
+                    }
+                }
+            }
+        }
+    }
+
+    private void win()
+    {
+        winPanel.gameObject.SetActive(true);
+    }
+
+}
